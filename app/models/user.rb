617cfd161fe3bf62bea30342b3ac84d2a6937386
Cagatay Soyer <cagataysoyer@gmail.com>
2020-10-19 23:32:22 +0300

    create a validations and associations for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+  has_many :bookings, dependent: :destroy
+  # discuss with Sy should we make a delete dependency here?
+  has_many :super_heroes # dependent: :destroy
+
+  validates :username, presence: true, uniqueness: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
