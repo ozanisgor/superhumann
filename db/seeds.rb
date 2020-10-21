291826a0fd7055c2723e3a7008f459427e49b852
leena khalili <engleena@yahoo.com>
2020-10-21 18:02:44 +0300

    dashboard

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+require 'date'
+
 puts "Cleaning user database..."
 User.destroy_all
 puts "Creating users..."
@@ -15,12 +17,17 @@ user2 = User.create( username: "username2", password: "123456", email: "email2@e
 puts "Finished!"
 
 
-puts "Cleaning cahampion database..."
+puts "Cleaning shampion database..."
 Champion.destroy_all
 puts "Creating champions..."
 the_hulk = Champion.create(name: "The Hulk", universe: "Earth-616 - Prime Marvel Universe", description: "Human-Radiation", rate: 10, user: user1)
 superman =  Champion.create( name: "superman", universe: "Prime Earth - Prime DC Comics Universe", description: "Kryptonian", rate: 20, user: user2)
 
+puts "Cleaning booking database..."
+Booking.destroy_all
+puts "creating bookings ..."
+10.times { booking = Booking.create(start_time:Date.parse('2001-02-03') , end_time:Date.parse('2001-02-03') , total_price: 554, user: user1, champion: the_hulk)}
 puts "Finished!"
 
+puts Booking.count
 
