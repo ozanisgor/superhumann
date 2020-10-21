5a5f37a44b74d22d65904040c7781037d6069b22
Cagatay Soyer <cagataysoyer@gmail.com>
2020-10-21 12:14:18 +0300

    booking routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :champions, only: [ :index, :new, :create ]
+  resources :bookings, only: [ :new, :create ]
 end
