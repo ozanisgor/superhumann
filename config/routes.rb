3c6973d6431f66b3bf9adb98cedb75f32e1bde3e
Cagatay Soyer <cagataysoyer@gmail.com>
2020-10-21 13:11:03 +0300

    routes added, create method added to booking controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :champions, only: [ :index, :new, :create ]
-  resources :bookings, only: [ :new, :create ]
+  resources :champions, only: [ :index, :new, :create ] do
+    resources :bookings, only: [ :new, :create ]
+  end
 end
