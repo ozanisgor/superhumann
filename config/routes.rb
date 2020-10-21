d1d510e1c865e70fb679d8f1fe277f8990a4d3ca
ozan isgor <ozanisgor11@gmail.com>
2020-10-21 12:14:02 +0300

    show route added for champions and route added for reviews (only create)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :champions, only: [ :index, :new, :create ]
+  resources :champions, only: [ :index, :new, :create, :show ] do 
+    resources :reviews, only: [ :create ]
+  end
 end
